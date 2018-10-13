27401264912a6542282e90b2ee97e826a41c61c5
braj011 <bhagyaraj01@gmail.com>
2018-10-13 16:26:23 +0100

    Done.

--- konami_code.js
+++ konami_code.js
@@ -9,8 +9,26 @@ const codes = [
   "ArrowRight",
   "b",
   "a"
-];
+]
 
 function init() {
-  // your code here
+  let index = 0
+  // create event listener
+  document.body.addEventListener("keydown", (ele) => {
+    const key = ele.key
+    // assign key to be the element's index number, i.e. the key (The buttons pressed are the values)
+
+    index = (key === codes[index]) ? ++index : 0                   
+    // can't redeclare the index with let or const
+    // ternary - if the right key is entered, index looks for next key,
+    // else if wrong key is entered, index goes back to 0 
+    // ++index - because we want the value of index after the increment
+
+    if  (index === codes.length) {
+      window.alert('CONGRATULATIONS GAMER!')
+      index = 0   // reset this to zero 
+    }
+  })
 }
+
+
